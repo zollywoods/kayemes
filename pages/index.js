8d2d1cf63ed9695935a22e4950f43e08992efe60
zollywoods <zooollywood@gmail.com>
2022-10-06 13:12:34 -0400

    fonts downloaded

--- pages/index.js
+++ pages/index.js
@@ -26,7 +26,7 @@ export default function Home() {
         <li className={styles.menuListItem}> Viewing Room </li>
         <li className={styles.menuListItem}> News </li>
         <li className={styles.menuListItem}> About </li>
-        <li className={styles.menuKayemes}> KAYEMES </li>
+        <li className={styles.menuKayemes}> KAYEMESTEXAS </li>
       </ul>
     </div>
     <hr className={styles.line}/>
