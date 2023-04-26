a65693f9f672a4d6b3059c812c834631c683e59f
zollywoods <zooollywood@gmail.com>
2023-04-25 17:23:50 -0700

    analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,14 @@
 import '../styles/globals.css'
+import { Analytics } from '@vercel/analytics/react'
+;
 // import '../styles/luke.modules.css'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  <> 
+    return <Component {...pageProps} />
+    <Analytics/>
+  </>
+
 }
 
 export default MyApp
