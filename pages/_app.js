ca498a5a9bd9e0703fce916f03ac9a896233ec6b
zollywoods <zooollywood@gmail.com>
2023-04-25 17:28:18 -0700

    the thing

--- pages/_app.js
+++ pages/_app.js
@@ -1,14 +1,29 @@
-import '../styles/globals.css'
+// import '../styles/globals.css'
 // import { Analytics } from '@vercel/analytics/react'
-;
-// import '../styles/luke.modules.css'
+// ;
+// // import '../styles/luke.modules.css'
 
+// function MyApp({ Component, pageProps }) {
+//   <> 
+//     return <Component {...pageProps} />
+//     <Analytics/>
+//   </>
+
+// }
+
+// export default MyApp
+
+import '../styles/globals.css'
+import { AppProps } from 'next/app';
+import { Analytics } from '@vercel/analytics/react';
+ 
 function MyApp({ Component, pageProps }) {
-  // <> 
-    return <Component {...pageProps} />
-    {/* <Analytics/> */}
-  // </>
-
+  return (
+    <>
+      <Component {...pageProps} />
+      <Analytics />
+    </>
+  );
 }
-
-export default MyApp
+ 
+export default MyApp;
